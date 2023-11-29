40c6c3bdc6173a612e1c5a9cce4ba4bd11ef00a2
Iaroslav <yaroslav.da@mail.ru>
2023-11-29 20:46:13 +0300

    add def f_sym and docstring def upper_text

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def upper_text(value):
   """Возвращает текст с заглавными буквами"""
   return value.upper()
+
+
+def f_sym(value):
+  """Возвращает каждое слово с заглавной буквы в тексте"""
+  return value.title()
